17e702a1f9960fd8bdd8328f553979b4e802ade7
Jess Merry <merryja.email@gmail.com>
2022-04-11 15:50:36 -0400

    search bar

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 console.log("music!")
 
-let tunesDiv = document.querySelector('#tunes')
-console.log('tunes')
+let kahunaDiv = document.querySelector('#kahuna')
 
-fetch('https://itunes.apple.com/search?term=freeyourselfup&entity=musicTrack',{
+let formBox = document.querySelector('#search')
+
+formBox.addEventListener ('submit', function(event) {
+    event.preventDefault()
+
+    let userInput = document.querySelector('#user-input')
+    console.log(userInput.value)
+
+
+fetch(`https://itunes.apple.com/search?term=${userInput.value}&media=music`,{
     method: 'GET',
     headers: {},
 })
@@ -11,9 +19,31 @@ fetch('https://itunes.apple.com/search?term=freeyourselfup&entity=musicTrack',{
     return response.json()
 })
 .then (function (data){
-    console.log(data)
-    let nameDiv = document.createElement('div')
-    nameDiv.classList.add('name')
-    nameDiv.innerText = `${data.artistName}`
-    tunesDiv.appendChild(nameDiv)
+
+    let tunesDiv = document.createElement('div')
+    tunesDiv.classList.add("tunes")
+
+    let results = data.results
+        for (let result of results) {
+        let pictureDiv = document.createElement('img')
+            pictureDiv.classList.add('img')
+            pictureDiv.src = result.artworkUrl100
+            tunesDiv.appendChild(pictureDiv)
+        }
+        for (let result of results) {
+            // console.log(i.trackName)
+        let nameDiv = document.createElement('div')
+            nameDiv.classList.add('track')
+            nameDiv.innerText = result.trackName
+            tunesDiv.appendChild(nameDiv)
+        }
+        for (let result of results) {        
+        let bandDiv = document.createElement('div')
+            bandDiv.classList.add('band')
+            bandDiv.innerText = result.artistName
+            tunesDiv.appendChild(bandDiv)
+
+        kahunaDiv.appendChild(tunesDiv)
+        }
+    })
 })
